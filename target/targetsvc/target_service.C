935a5e8e681f736b1f4400be9dc1c1fdce4ebd63
Gopichand Paturi <gopichandpaturi@gmail.com>
2025-08-22 01:37:42 -0500

    targetsvc: Add setHwAccessMethod to set access method for a target
    setHwAccessMethod shall be used by the user application to set if
    the target uses DirectAccess or SBEFifo Access for subsequent calls
    to getscom, putscom etc.
    
    Signed-off-by: Gopichand Paturi <gopichandpaturi@gmail.com>

--- target/targetsvc/target_service.C
+++ target/targetsvc/target_service.C
@@ -3,6 +3,7 @@ extern "C"
 {
 #include <libfdt.h>
 }
+#include <hwAccessIntf.H>
 
 #include <fstream>
 #include <vector>
@@ -66,5 +67,17 @@ TargetPtr TargetService::getTopLevelTarget() const
         return nullptr;
     }
     return _targetMap->getTopLevelTarget();
+}
+
+bool TargetService::setHwAccessMethod(TargetPtr target, HwAccessMethod accessMethod)
+{
+    if (target->trySetAttr<ATTR_HW_ACCESS_METHOD>(accessMethod))
+    {
+        uintptr_t accessPtr =
+            reinterpret_cast<uintptr_t>(HWACCESS::HwAccessIntf::getHwAccessPtr(accessMethod));
+
+        return target->trySetAttr<ATTR_HW_ACCESS_PTR>(static_cast<HwAccessPtrType>(accessPtr));
+    }
+    return false;
 }
 } // namespace TARGETING
