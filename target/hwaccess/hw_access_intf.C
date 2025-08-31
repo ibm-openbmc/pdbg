005592ba676f1a9edd754cdcf6527ecbea76ab49
Gopichand Paturi <gopichandpaturi@gmail.com>
2025-08-31 00:58:16 -0500

    hwaccess: Create hwAccess library classes & methods
    1. Added new hwAccess library which will be the interface to access
       hwTransport layer APIs that do direct_fsi/sbe_fifo access.
    
    2. Added 5 interface methods for external applications
       getcfam, putcfam, getscom, putscom, gethwaccessptr
    
    3. Added setHwAccessMethod to set access method for a target
    
    Signed-off-by: Gopichand Paturi <gopichandpaturi@gmail.com>

---
+++ target/hwaccess/hw_access_intf.C
@@ -0,0 +1,103 @@
+#include <hw_access_intf.H>
+#include "hw_base_access.H"
+#include "hw_direct_access.H"
+#include "hw_sbe_access.H"
+
+namespace hwaccess
+{
+using HwAccessPtr = TARGETING::AttributeTraits<TARGETING::ATTR_HW_ACCESS_PTR>::Type;
+
+int HwAccessIntf::getCfamRegister(ConstTargetPtr_t target, uint32_t addr, uint32_t& val)
+{
+    if(target == nullptr)
+    {
+        std::cerr << "getCfamRegister: target is nullptr" << std::endl;
+        return -1;
+    }
+
+    auto accessPtr = target->getAttr<TARGETING::ATTR_HW_ACCESS_PTR>();
+    HwBaseAccess* hwAccessPtr = reinterpret_cast<HwBaseAccess*>(static_cast<HwAccessPtr>(accessPtr));
+
+    if (hwAccessPtr == nullptr)
+    {
+        std::cerr << "getCfamRegister: hwAccessPtr is nullptr" << std::endl;
+        return -1;
+    }
+
+    return hwAccessPtr->getCfam(target, addr, val);
+}
+
+int HwAccessIntf::putCfamRegister(ConstTargetPtr_t target, uint32_t addr, uint32_t val)
+{
+    if(target == nullptr)
+    {
+        std::cerr << "putCfamRegister: target is nullptr" << std::endl;
+        return -1;
+    }
+
+    auto accessPtr = target->getAttr<TARGETING::ATTR_HW_ACCESS_PTR>();
+    HwBaseAccess* hwAccessPtr = reinterpret_cast<HwBaseAccess*>(static_cast<HwAccessPtr>(accessPtr));
+
+    if (hwAccessPtr == nullptr)
+    {
+        std::cerr << "putCfamRegister: hwAccessPtr is nullptr" << std::endl;
+        return -1;
+    }
+
+    return hwAccessPtr->putCfam(target, addr, val);
+}
+
+int HwAccessIntf::getScomRegister(ConstTargetPtr_t target, uint64_t addr, uint64_t& val)
+{
+    if(target == nullptr)
+    {
+        std::cerr << "getScomRegister: target is nullptr" << std::endl;
+        return -1;
+    }
+
+    auto accessPtr = target->getAttr<TARGETING::ATTR_HW_ACCESS_PTR>();
+    HwBaseAccess* hwAccessPtr = reinterpret_cast<HwBaseAccess*>(static_cast<HwAccessPtr>(accessPtr));
+
+    if (hwAccessPtr == nullptr)
+    {
+        std::cerr << "getScomRegister: hwAccessPtr is nullptr" << std::endl;
+        return -1;
+    }
+
+    return hwAccessPtr->getScom(target, addr, val);
+}
+
+int HwAccessIntf::putScomRegister(ConstTargetPtr_t target, uint64_t addr, uint64_t val)
+{
+    if(target == nullptr)
+    {
+        std::cerr << "putScomRegister: target is nullptr" << std::endl;
+        return -1;
+    }
+
+    auto accessPtr = target->getAttr<TARGETING::ATTR_HW_ACCESS_PTR>();
+    HwBaseAccess* hwAccessPtr = reinterpret_cast<HwBaseAccess*>(static_cast<HwAccessPtr>(accessPtr));
+
+    if (hwAccessPtr == nullptr)
+    {
+        std::cerr << "putScomRegister: hwAccessPtr is nullptr" << std::endl;
+        return -1;
+    }
+
+    return hwAccessPtr->putScom(target, addr, val);
+}
+
+void* HwAccessIntf::getHwAccessPtr(HwAccessMethod& accessMethod)
+{
+    if(accessMethod == TARGETING::HW_ACCESS_METHOD_DIRECT_ACCESS)
+    {
+        return static_cast<void*>(&HwDirectAccess::getInstance());
+    }
+    else if(accessMethod == TARGETING::HW_ACCESS_METHOD_SBEFIFO)
+    {
+        return static_cast<void*>(&HwSbeAccess::getInstance());
+    }
+    std::cerr << "getHwAccessPtr: HwAccessMethod is invalid" << std::endl;
+    return nullptr;
+}
+} //namespace hwaccess
